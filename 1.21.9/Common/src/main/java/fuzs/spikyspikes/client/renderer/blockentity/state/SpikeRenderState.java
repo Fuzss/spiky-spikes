92ed5d358917199f4e688e4fcbdb5e78d51d47e5
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-08 22:27:34 +0200

    prepare 1.21.10 port

--- 1.21.9/Common/src/main/java/fuzs/spikyspikes/client/renderer/blockentity/state/SpikeRenderState.java
+++
@@ -1,7 +0,0 @@
-package fuzs.spikyspikes.client.renderer.blockentity.state;
-
-import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
-
-public class SpikeRenderState extends BlockEntityRenderState {
-    public int blockColor;
-}
